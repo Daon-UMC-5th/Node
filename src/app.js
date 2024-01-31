7e5db3d7db56b5ad67e42d4493589c83474aa481
dareunk <happyeunnn11@gmail.com>
2024-01-31 20:09:52 +0900

    feat: update the router for authorization by email and library

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const SwaggerUi = require("swagger-ui-express");
+const expressSession = require("express-session");
+const memoryStore = require("memorystore")(expressSession);
 //# 라이브러리 import
 
 //@  폴더 파일 import
@@ -15,7 +17,7 @@ const { specs } = require("./config/swaggerConfig.js");
 
 //@ 라우터
 const tempRouter = require("./routes/tempRoute");
-
+const authRouter = require("./routes/authRoute.js");
 //# 라우터
 
 //@ app 설정 공간
@@ -29,12 +31,22 @@ app.use(cors());
 
 // JSON 형식의 요청 데이터 파싱, URL 인코딩된 데이터를 파싱, 요청에 포함된 쿠키를 파싱
 app.use(express.json());
+
+app.use(cookieParser("secret_password"));
+app.use(expressSession({
+	secret: "secret_password",
+	cookie:{
+		maxAge:4000000
+	},
+	resave:false,
+	saveUninitialized:true,
+	store: new memoryStore()
+}));
 app.use(
-  express.urlencoded({
-    extended: false,
-  })
+	express.urlencoded({
+	  extended: true
+	})
 );
-app.use(cookieParser());
 
 //# app 설정 공간
 
@@ -49,7 +61,7 @@ app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(specs));
 
 // 실제로 작동,  테스트 한 후 지우기
 app.use("/temp", tempRouter);
-
+app.use("/auth", authRouter);
 //# 라우트
 
 //@ 서버 실행
