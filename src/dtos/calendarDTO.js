d4a034b45ffca0166f00a914a3790e7cfa4a4c58
dareunk <happyeunnn11@gmail.com>
2024-02-06 00:40:35 +0900

    feat: add the dto for the consultation in calendar

--- src/dtos/calendarDTO.js
+++ src/dtos/calendarDTO.js
@@ -1,4 +1,4 @@
-// 배열을 객체로 전환 
+// 배열을 객체로 전환 in 신체 
 const getPhysicalRecordDTO = async(result) => {
 
 let object = {
@@ -18,3 +18,22 @@ return object;
 
 };
 module.exports.getPhysicalRecordDTO = getPhysicalRecordDTO;
+
+// 배열을 객체로 전환 in 진료
+const getConsultationDTO = async(result) => {
+
+let object = {
+    "consultation_id" : result[0].consultation_id,
+    "user_id" : result[0].user_id,
+    "hospital" : result[0].hospital,
+    "content": result[0].content,
+    "alarmed_date": result[0].alarmed_date,
+    "alarmed_at": result[0].alarmed_at,
+    "created_at" : result[0].created_at,
+    "updated_at": result[0].updated_at
+
+};
+console.log(object);
+return object;
+}
+module.exports.getConsultationDTO = getConsultationDTO;
