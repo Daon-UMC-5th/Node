5af784c0846ccaf8a23ce9b541676a337ad17ae0
dareunk <happyeunnn11@gmail.com>
2024-02-06 00:42:09 +0900

    feat: update the router for the consultation in calendar

--- src/routes/calendarRoute.js
+++ src/routes/calendarRoute.js
@@ -2,23 +2,27 @@ const express = require("express");
 const calendarRouter = express.Router();
 const calendarController = require("../controllers/calendarController.js");
 
-// 날짜 별 진료 목록 조회 
-//calendarRouter.get("/consultation/:date", );
-// 날짜 별 진료 목록 등록
-//calendarRouter.post("/consultation/:date",);
-// 날짜 별 진료 목록 삭제
+// 날짜별 진료 목록 조회 
+calendarRouter.get("/consultation/:date", calendarController.viewConsultation);
+// 날짜별 진료 목록 등록
+calendarRouter.post("/consultation/:date",calendarController.registerConsultation);
+// 날짜별 진료 목록 삭제
+calendarRouter.delete("/consultation/:date", calendarController.deleteConsultation);
+// 날짜별 진료 목록 수정
+calendarRouter.put("/consultation/:date", calendarController.updateConsultation);
 
-/*
+
+// 날짜별 
 calendarRouter.get("/medication/:{date}",);
 calendarRouter.post("/consultation/:{date}",);
 */
 
 // 날짜별 신체 목록 조회 
-calendarRouter.get("/physicalRecord/:date", calendarController.viewPhysicalRecord);
+calendarRouter.get("/physical-record/:date", calendarController.viewPhysicalRecord);
 // 날짜별 신체 목록 등록
-calendarRouter.post("/physicalRecord/register/:date", calendarController.registerPhysicalRecord);
+calendarRouter.post("/physical-record/:date", calendarController.registerPhysicalRecord);
 // 날짜별 신체 목록 삭제
-calendarRouter.get("/physicalRecord/delete/:date", calendarController.deletePhysicalRecord);
+calendarRouter.delete("/physical-record/:date", calendarController.deletePhysicalRecord);
 // 날짜별 신체 목록 수정
-calendarRouter.post("/physicalRecord/update/:date", calendarController.updatePhysicalRecord);
+calendarRouter.put("/physical-record/:date", calendarController.updatePhysicalRecord);
 module.exports = calendarRouter;
