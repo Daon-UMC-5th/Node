73406f22d975cb20fe480296f812c226fdf9655c
dareunk <happyeunnn11@gmail.com>
2024-02-06 00:43:07 +0900

    feat: add the service for the consultation in calendar

--- src/services/calendarService.js
+++ src/services/calendarService.js
@@ -1,24 +1,42 @@
-// 신체 기록 삽입,조회,삭제,수정을 위한 dao
+// 신체기록 삽입,조회,삭제,수정
 const { insertPhysicalRecordInDB, checkPhysicalRecordInDB,deletePhysicalRecordInDB, updatePhysicalRecordInDB} = require("../models/calendarDAO.js");
 const {getPhysicalRecordDTO} = require("../dtos/calendarDTO.js");
+const {checkDuplicationDateInDBInPhysical} = require("../models/calendarDAO.js");
 
+// 진료기록 삽입,조회,삭제,수정
+const {insertConsultationInDB, checkConsultationInDB, deleteConsultationInDB,updateConsultationInDB} = require("../models/calendarDAO.js");
+const {getConsultationDTO} = require("../dtos/calendarDTO.js");
+const {checkDuplicationDateInDBInConsultation} = require("../models/calendarDAO.js");
 
-const {checkDuplicationDateDateInDB} = require("../models/calendarDAO.js");
+// 같은 사용자가 같은 날짜에 중복으로 작성하는지를 체크하는 함수 in physical_record
 
-// 같은 사용자가 같은 날짜에 중복으로 작성하는지를 체크하는 함수
-
-const checkDuplicationDate = async(date,userId) => {
+const checkDuplicationDateInPhysical = async(date,userId) => {
 
     let isDuplication;
-    const result = await checkDuplicationDateDateInDB(date,userId);
+    const result = await checkDuplicationDateInDBInPhysical(date,userId);
     console.log(`result: ${result}`);
     // 함수 실행 결과, 동일한 user_id와 date가 이미 기입되어 있는 경우(중복)
     if(result != undefined) isDuplication = true;
     else isDuplication = false;
     return isDuplication;
 }
-module.exports={
 
+
+// 같은 사용자가 같은 날짜에 중복으로 작성하는지를 체크하는 함수 in consultation
+const checkDuplicationDateInConsultation = async(date,userId) => {
+
+    let isDuplication;
+    const result = await checkDuplicationDateInDBInConsultation(date,userId);
+    console.log(`result: ${result}`);
+    // 함수 실행 결과, 동일한 user_id와 date가 이미 기입되어 있는 경우(중복) 
+    if(result != undefined) isDuplication = true;
+    else isDuplication = false;
+    return isDuplication;
+}
+
+module.exports={
+    
+    // 신체 
     insertPhysicalRecord: async(information) => {
         
         const data = {
@@ -30,7 +48,7 @@ module.exports={
             special_note: information.note
         }
     // 중복 체크
-    let isDuplication = await checkDuplicationDate(data.alarmed_date, data.user_id);
+    let isDuplication = await checkDuplicationDateInPhysical(data.alarmed_date, data.user_id);
     console.log(`isDuplication: ${isDuplication}`);
     if(isDuplication){
         const result = "duplication";
@@ -75,6 +93,67 @@ module.exports={
         //console.log(result);
         if(result.affectedRows==0) return undefined;
         else return result;
-    }
+    },
    
+
+    // 진료
+    // 진료기록 삽입 
+    insertConsultation: async(information) => {
+
+        const data = {
+            user_id: information.userId,
+            hospital: information.hospital,
+            content: information.content,
+            alarmed_date: information.date,
+            alarmed_at: information.alarmed_at
+        };
+
+        // 중복체크 
+        let isDuplication = await checkDuplicationDateInConsultation(data.alarmed_date, data.user_id);
+        if(isDuplication){
+            const result = "duplication";
+            return result;
+        }else{
+            const result = await insertConsultationInDB(data);
+    
+            if(result) return result;
+            else return undefined;
+        }
+    },
+    // 진료기록 조회
+    getConsultation: async(date, userId) => {
+
+        const result = await checkConsultationInDB(date,userId);
+
+         // 서버 에러
+         if(result == "error") return result;
+         else if(result.length != 0) return getConsultationDTO(result);
+         else return undefined;
+    },
+    removeConsultation: async(date, userId) => {
+
+        const result = await deleteConsultationInDB(date,userId);
+
+        // 해당 사용자, 날짜에 존재하는 진료기록이 없는 경우(영향을 받은 row가 0)
+        if(result.affectedRows==0) return undefined;
+        else return result;
+    },
+    modifyConsultation: async(date, modification) => {
+
+        const data = {
+            user_id: modification.userId,
+            hospital: modification.hospital,
+            content: modification.content,
+            alarmed_date: modification.date,
+            alarmed_at: modification.alarmed_at
+        }
+        //console.log(data.hospital);
+        const result = await updateConsultationInDB(date,data);
+
+        // 수정할 게시글이 존재하지 않는 경우(update에 영향받는 row가 0)
+        if(result.affectedRows ==0) return undefined;
+        else return result;
+
+    }
+
 };
