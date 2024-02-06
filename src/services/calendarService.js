f21e47f259ab596c35cfb73971dff6e3923e0e3c
dareunk <happyeunnn11@gmail.com>
2024-02-07 00:38:36 +0900

    feat: add the service for the record of medication

--- src/services/calendarService.js
+++ src/services/calendarService.js
@@ -8,6 +8,11 @@ const {insertConsultationInDB, checkConsultationInDB, deleteConsultationInDB,upd
 const {getConsultationDTO} = require("../dtos/calendarDTO.js");
 const {checkDuplicationDateInDBInConsultation} = require("../models/calendarDAO.js");
 
+// 복용기록 삽입,조회,삭제,수정
+const {checkMedicationInDB,checkAllMedicationInDB,insertMedicationInDB, deleteMedicationInDB, updateMedicationInDB} = require("../models/calendarDAO.js");
+const {getMedicationDTO, getAllMedicationDTO} = require("../dtos/calendarDTO.js");
+const {checkDuplicationDateInDBInMedication} = require("../models/calendarDAO.js");
+
 // 같은 사용자가 같은 날짜에 중복으로 작성하는지를 체크하는 함수 in physical_record
 
 const checkDuplicationDateInPhysical = async(date,userId) => {
@@ -33,6 +38,17 @@ const checkDuplicationDateInConsultation = async(date,userId) => {
     else isDuplication = false;
     return isDuplication;
 }
+
+// 같은 사용자가 같은 날짜에 중복으로 작성하는지를 체크하는 함수 in medication
+const checkDuplicationDateInMedication = async(date, timeOfDay, userId) =>{
+
+    let isDuplication;
+    const result = await checkDuplicationDateInDBInMedication(date,timeOfDay,userId);
+    if(result != undefined) isDuplication = true;
+    else isDuplication = false;
+    return isDuplication;
+}
+
 
 module.exports={
     
@@ -154,6 +170,99 @@ module.exports={
         if(result.affectedRows ==0) return undefined;
         else return result;
 
+    },
+    getAllMedication: async(date, userId) =>{
+
+        const result = await checkAllMedicationInDB(date, userId);
+
+          // 서버 에러
+          if(result == "error") return result;
+          else if(result.length != 0) return getAllMedicationDTO(result);
+          else return undefined;
+    },
+    getMedication: async(when, date, userId) => {
+
+        let timeOfDay;
+
+        if(when == "morning") timeOfDay = "아침";
+        else if(when == "noon") timeOfDay = "점심";
+        else if(when == "evening") timeOfDay ="저녁";
+      
+
+        const result = await checkMedicationInDB(timeOfDay,date,userId);
+
+          // 서버 에러
+          if(result == "error") return result;
+          else if(result.length != 0) return getMedicationDTO(result);
+          else return undefined;
+    },
+    insertMedication: async(information) => {
+
+        let timeOfday;
+
+        if(information.time_of_day == "morning") timeOfday = "아침";
+        else if(information.time_of_day == "noon") timeOfday = "점심";
+        else if(information.time_of_day == "evening") timeOfday = "저녁"; 
+
+        const data = {
+
+            user_id : information.userId,
+            alarmed_date: information.alarmed_date,
+            time_of_day: timeOfday,
+            medicine: information.medicine,
+            alarmed_at: information.alarmed_at,
+            alarm_days: information.alarm_days,
+            repeat_status: information.repeat_status
+        };
+
+        let isDuplication = await checkDuplicationDateInMedication(data.alarmed_date, timeOfday, data.user_id);
+        if(isDuplication){
+            const result = "duplication";
+            return result;
+        }else{
+            const result = await insertMedicationInDB(data);
+            if(result) return result;
+            else return undefined;
+        }
+    },
+    removeMedication: async(date, when, userId) => {
+
+        let timeOfDay;
+
+        if(when == "morning") timeOfDay = "아침";
+        else if(when == "noon") timeOfDay = "점심";
+        else if(when == "evening") timeOfDay ="저녁";
+
+        const result = await deleteMedicationInDB(date, timeOfDay, userId);
+        console.log(result);
+        if(result.affectedRows == 0) return undefined;
+        else return result;
+    },
+    modifyMedication: async(modification) =>{
+
+        let timeOfday;
+
+        if(modification.time_of_day == "morning") timeOfday = "아침";
+        else if(modification.time_of_day == "noon") timeOfday = "점심";
+        else if(modification.time_of_day == "evening") timeOfday = "저녁";
+        
+        const data = {
+
+            user_id : modification.userId,
+            alarmed_date: modification.alarmed_date,
+            time_of_day: timeOfday,
+            medicine: modification.medicine,
+            alarmed_at: modification.alarmed_at,
+            alarm_days: modification.alarm_days,
+            repeat_status: modification.repeat_status
+        }
+
+        const result = await updateMedicationInDB(data);
+
+        // 수정할 게시글이 존재하지 않는 경우(update에 영향받는 row가 0)
+        if(result.affectedRows == 0) return undefined;
+        else return result;
     }
+    
 
 };
