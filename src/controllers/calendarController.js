2f538588fca4dc801c1678503a4cf37fcaa29eb5
dareunk <happyeunnn11@gmail.com>
2024-02-07 00:36:05 +0900

    feat: add the controller for the record of medication in calendar

--- src/controllers/calendarController.js
+++ src/controllers/calendarController.js
@@ -207,5 +207,144 @@ module.exports = {
         else if(result) res.send(response(status.SUCCESS));
         // 존재하지 않는 신체기록을 수정하려는 경우 
         else res.send(response(status.ARTICLE_NOT_FOUND));
+    },
+
+    // 날짜별 복용목록 모두 조회 
+    viewAllMedication: async(req,res,next) => {
+        // 사용자의 userid 받아옴
+        const userId = 3;
+
+        // 아침, 점심, 저녁
+        const date = req.params.date;
+
+        const result = await calendarService.getAllMedication(date,userId);
+
+        console.log(result);
+
+          // 서버 에러로 조회 실패
+          if(result == "error") res.send(response(status.INTERNAL_SERVER_ERROR));
+          // 해당 날짜에 복용 기록이 없는 경우
+          else if(result==undefined) res.send(response(status.ARTICLE_NOT_FOUND));
+          // 해당 날짜에 복용 기록이 있는 경우
+          else res.send(response(status.SUCCESS, result));
+    },
+
+    // 복용
+    // 날짜별 복용목록 아침/점심/저녁 별 조회
+    viewMedication: async(req,res,next) => {
+
+        // 사용자의 userid 받아옴
+        const userId = 3;
+
+        //아침, 점심, 저녁
+        const when = req.params.when;
+        // 사용자가 복용기록을 조회하고자 하는 날짜
+        const date = req.params.date;
+        
+        const result = await calendarService.getMedication(when,date,userId);
+        
+        console.log(result);
+        // 서버 에러로 조회 실패
+        if(result == "error") res.send(response(status.INTERNAL_SERVER_ERROR));
+        // 해당 날짜에 복용 기록이 없는 경우
+        else if(result==undefined) res.send(response(status.ARTICLE_NOT_FOUND));
+        // 해당 날짜에 복용 기록이 있는 경우
+        else res.send(response(status.SUCCESS, result));
+        
+    },
+    
+    // 날짜별 복용목록 삽입 
+    registerMedication: async(req,res,next) => {
+
+    // 사용자의 userid 받아옴
+    const userId = 3;
+
+    //아침, 점심, 저녁
+     const when = req.params.when;
+     // 사용자가 복용기록을 조회하고자 하는 날짜
+    const date = req.params.date;
+
+    //repeat_status 판단
+    let isRepeat;
+    if(req.body.alarm_days) isRepeat = true;
+    else isRepeat = false;
+    
+    const userInformation = {
+        userId: userId,
+        alarmed_date: date,
+        time_of_day: when,
+        medicine: req.body.medicine,
+        alarmed_at: req.body.alarmed_at,
+        alarm_days: req.body.alarm_days,
+        repeat_status: isRepeat
+    };
+
+    const result = await calendarService.insertMedication(userInformation);
+
+    
+    if(result=="duplication") res.send(response(status.ARTICLE_DUPLICATION));
+    else{
+        if(result) res.send(response(status.SUCCESS));
+        else res.send(response(status.INTERNAL_SERVER_ERROR));
     }
+    },
+    
+    //날짜별 복용목록 삭제
+    deleteMedication: async(req,res,next) => {
+        
+        // 사용자의 userid 받아옴
+        const userId = 3;
+
+        // 사용자가 신체기록을 삭제하고자 하는 부분과 날짜
+        const  when = req.params.when;
+        const date = req.params.date;
+
+        const result = await calendarService.removeMedication(date,when,userId);
+
+
+        // 서버 에러로 삭제 실패 
+         if(result=="error") res.send(response(status.INTERNAL_SERVER_ERROR));
+        // 삭제 성공 
+         else if(result != undefined) res.send(response(status.SUCCESS));
+        // 존재하지 않는 신체기록을 삭제하려는 경우 
+         else res.send(response(status.ARTICLE_NOT_FOUND));
+  
+
+
+    },
+    //날짜별 복용목록 수정 
+    updateMedication: async(req,res,next) => {
+     
+        // 사용자의 userid 받아옴
+        const userId = 3;
+
+        // 사용자가 신체기록을 수정하고자 하는 부분과 날짜
+        const when = req.params.when;
+        const date = req.params.date;
+
+        //repeat_status 판단
+        let isRepeat;
+        if(req.body.alarm_days) isRepeat = true;
+        else isRepeat = false;
+    
+    const userInformation = {
+        userId: userId,
+        alarmed_date: date,
+        time_of_day: when,
+        medicine: req.body.medicine,
+        alarmed_at: req.body.alarmed_at,
+        alarm_days: req.body.alarm_days,
+        repeat_status: isRepeat
+    };
+        const result = await calendarService.modifyMedication(userInformation);
+
+        // 서버 에러로 수정 실패
+        if(result =="error") res.send(response(status.INTERNAL_SERVER_ERROR));
+        // 수정 성공 
+        else if(result) res.send(response(status.SUCCESS));
+        // 존재하지 않는 복용기록을 수정하려는 경우 
+        else res.send(response(status.ARTICLE_NOT_FOUND));
+        
+    }
+
 };
