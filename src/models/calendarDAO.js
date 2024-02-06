4c5dcedcd792f0af02548f861ffbf6dbbd28a08e
dareunk <happyeunnn11@gmail.com>
2024-02-07 00:37:05 +0900

    feat: update the database model for the record of medication

--- src/models/calendarDAO.js
+++ src/models/calendarDAO.js
@@ -8,6 +8,10 @@ const {checkDuplicationDateInPhysicalSQL} = require("./calendarSQL.js");
 const {insertConsultationSQL, getConsultationSQL,deleteConsultationSQL, updateConsultationSQL} = require("./calendarSQL.js");
 const {checkDuplicationDateInConsultationSQL} = require("./calendarSQL.js");
 
+// 복용
+const {getMedicationSQL,getAllMedicationSQL,insertMedicationSQL, deleteMedicationSQL, updateMedicationSQL} = require("./calendarSQL.js");
+const {checkDuplicationDateInMedicationSQL} = require("./calendarSQL.js");
+
 // 신체기록 삽입 
 const insertPhysicalRecordInDB = async(data) => {
 
@@ -179,7 +183,7 @@ const updateConsultationInDB = async(date, data) => {
         console.log(data.hospital);
         const result = await db.query(updateConsultationSQL, [data.hospital, data.content, data.alarmed_at, date, data.user_id]);
         dbConnection.release();
-
+        console.log(result[0]);
         return result[0];
 
     }catch(err){
@@ -193,3 +197,116 @@ const updateConsultationInDB = async(date, data) => {
     }
 }
 module.exports.updateConsultationInDB = updateConsultationInDB;
+
+
+
+//복용
+// 복용기록 전체 조회
+const checkAllMedicationInDB = async(date, userId) => {
+    const dbConnection = await db.getConnection();
+
+    try{
+        const result = await db.query(getAllMedicationSQL, [date, userId]);
+
+       // console.log(result);
+        dbConnection.release();
+        return result[0];
+
+    }catch(err){
+        const result = [];
+        result[0] = "error";
+        dbConnection.release();
+        return result[0];
+    }
+}
+module.exports.checkAllMedicationInDB =  checkAllMedicationInDB;
+
+
+// 복용기록 조회
+const checkMedicationInDB = async(timeOfDay, date, userId) => {
+    console.log(timeOfDay);
+    const dbConnection = await db.getConnection();
+
+    try{
+        const result = await db.query(getMedicationSQL, [date,timeOfDay,userId]);
+        console.log(result);
+        dbConnection.release();
+        return result[0];
+    }catch(err){
+        const result = [];
+        result[0] = "error";
+        dbConnection.release();
+        return result[0];
+    }
+}
+module.exports.checkMedicationInDB = checkMedicationInDB;
+
+
+const checkDuplicationDateInDBInMedication = async(date, timeOfDay, userId) => {
+
+    const dbConnection = await db.getConnection();
+
+    const result = await db.query(checkDuplicationDateInMedicationSQL, [date, timeOfDay, userId]);
+
+    if(result[0].length !=0) return result[0];
+    else return undefined;
+}
+module.exports.checkDuplicationDateInDBInMedication = checkDuplicationDateInDBInMedication;
+
+
+const insertMedicationInDB = async(data) => {
+
+    const dbConnection = await db.getConnection();
+
+    const result = await db.query(insertMedicationSQL, [data.user_id, data.alarmed_date, data.time_of_day, data.medicine, data.alarmed_at, data.alarm_days, data.repeat_status]);
+
+    console.log(result[0]);
+
+    dbConnection.release();
+
+    return result[0];
+}
+module.exports.insertMedicationInDB = insertMedicationInDB;
+
+
+// 복용기록 삭제
+const deleteMedicationInDB = async(date, timeOfDay, userId) => {
+
+    const dbConnection = await db.getConnection();
+
+    try{
+        const result = await db.query(deleteMedicationSQL, [date, timeOfDay, userId]);
+        dbConnection.release();
+        console.log(result[0]);
+
+        return result[0];
+    }catch(err){
+        const result = [];
+        result[0] = "error";
+        dbConnection.release();
+
+        return result[0];
+    }
+}
+module.exports.deleteMedicationInDB = deleteMedicationInDB;
+
+// 복용기록 수정
+const updateMedicationInDB = async(data) => {
+
+    const dbConnection = await db.getConnection();
+
+    try{
+        const result = await db.query(updateMedicationSQL,[data.medicine, data.alarmed_at, data.alarm_days,data.repeat_status, data.alarmed_date, data.time_of_day, data.user_id]);
+        dbConnection.release();
+
+        return result[0];
+
+    }catch(err){
+        const result = [];
+        result[0] = "error";
+        dbConnection.release();
+
+        return result[0];
+    }
+}
+module.exports.updateMedicationInDB = updateMedicationInDB;
