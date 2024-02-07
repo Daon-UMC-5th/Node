8a0646a2d74d96302e1022badd16fd8a3ab1027b
grapyju <yoonju203@naver.com>
2024-02-08 01:50:27 +0900

    add user_id with error correction

--- src/routes/diaryRoute.js
+++ src/routes/diaryRoute.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { getPrivateDiary, getPublicDiary, getOneDiary, postDiary, putDiary, deleteDiary, diaryLikeUp, diaryLikeDown} = require('../controllers/diaryController.js')
 const jwtMiddleware = require("./../config/jwtMiddleware.js");
-
 const diaryRouter = express.Router();
+const response = require('../config/response.js');
+const status = require('../config/responseStatus.js');
 
-diaryRouter.use(jwtMiddleware, (req,res,next) => {
+diaryRouter.use(jwtMiddleware,(req,res,next) => {
     req.user_id = req.verifiedToken.user_id;
-    next();
+    if(req.user_id!==0){next();}
+    else{res.send(response(status.MEMBER_NOT_FOUND))}
 })
+
 
 //게시판 전체 가져오기
 diaryRouter.get('/get-private', (req, res) => {
